c934c0995c40902d7d00fdc45c3aa93aedd5980b
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-09-23 22:02:14 +0530

    fixed me query

--- src/resolvers/queries/me.ts
+++ src/resolvers/queries/me.ts
@@ -1,22 +1,36 @@
+import { verifyJWT } from '../../jwt'
 import { ResolverContext } from 'src/typings'
 import { UserResponse } from '../resolvertypes'
-import { serialize } from 'cookie'
 
 export async function me(
     _: any,
     __: any,
-    ___: ResolverContext,
+    ctx: ResolverContext,
 ): Promise<UserResponse> {
-    return {
-        user: {
-            name: 'aditya',
-            userId: 'e847tyr84-34ctr8734t-43rt83478r',
-            userType: 'CRE',
-            email: 'arkumawat78@gmail.com',
-            phoneNumber: '9351101486',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
-        error: null,
+    try {
+        // @ts-ignore
+        const cookie = ctx.request.cookies.mess_manager as string
+
+        // decode JWT received from client
+        const { payload, expired } = verifyJWT(cookie)
+
+        if (expired || !payload) throw new Error('not authenticated')
+
+        const { sessionId } = payload as { sessionId: string }
+
+        // find session by sessionId
+        const session = (await ctx.redis.get(`session-${sessionId}`)) as {
+            sessionId: string
+            userId: string
+        }
+
+        const user = await ctx.prisma.user.findFirst({
+            where: { userId: session.userId },
+        })
+        if (!user) throw new Error('user not found, session expired')
+
+        return { user, error: null }
+    } catch (error) {
+        return { user: null, error: error.message }
     }
 }
